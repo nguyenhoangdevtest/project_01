37585ef75f586d505ffd24f1e73eeba7398f4cab
Sy Hoang Nguyen <info@benlucitm.com>
2022-02-24 16:22:29 +0100

    first commit

---
+++ libs/models/src/lib/models.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ModelsProps {}
+
+export function Models(props: ModelsProps) {
+  return (
+    <div>
+      <h1>Welcome to Models!</h1>
+    </div>
+  );
+}
+
+export default Models;
---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  content: ['./apps/**/*.{js,ts,jsx,tsx}', './libs/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          300: '#e03232',
+        },
+        blue: {
+          700: '#3c90b8',
+          900: '#29637E',
+        },
+        gray: {
+          100: '#FAF8F6',
+          200: '#f2f1ef',
+          300: '#e6e6e6',
+          700: '#888787',
+          800: '#000000D9',
+          900: '#1A1818',
+        },
+      },
+      flex: {
+        1.25: '1.25 1.25 0%',
+        1.5: '1.5 1.5 0%',
+        2: '2 2 0%',
+      },
+      minHeight: {
+        96: '384px',
+      },
+    },
+    screens: {
+      sm: '576px',
+      // => @media (min-width: 576px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '992px',
+      // => @media (min-width: 992px) { ... }
+
+      xl: '1200px',
+      // => @media (min-width: 1200px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    borderColor: (theme) => ({
+      ...theme('colors'),
+      DEFAULT: theme('colors.gray.300', '#6D6F72'),
+    }),
+  },
+  plugins: [],
+};
